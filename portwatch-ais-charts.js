fd4a9ee5b34c6e904f5048b5eab47cabe7dbe46e
Alessandra Sozzi <alessandra.sozzi@gmail.com>
2025-04-30 07:27:21 -0400

    Update portwatch-ais-charts.js

--- portwatch-ais-charts.js
+++ portwatch-ais-charts.js
@@ -1,65 +1,84 @@
 const shift = 365; // Changed from 52*7: 52*7days - to keep comparing mondays with mondays, tuesdays with tuesdays, etc
 const ma = 7;
 
-var downloadSymbol = function(x, y, w, h) {
-    const path = [
-        // Arrow stem
-        'M', x + w * 0.5, y,
-        'L', x + w * 0.5, y + h * 0.7,
-        // Arrow head
-        'M', x + w * 0.3, y + h * 0.5,
-        'L', x + w * 0.5, y + h * 0.7,
-        'L', x + w * 0.7, y + h * 0.5,
-        // Box
-        'M', x, y + h * 0.9,
-        'L', x, y + h,
-        'L', x + w, y + h,
-        'L', x + w, y + h * 0.9
-    ];
-    return path;
+var downloadSymbol = function (x, y, w, h) {
+  const path = [
+    // Arrow stem
+    "M",
+    x + w * 0.5,
+    y,
+    "L",
+    x + w * 0.5,
+    y + h * 0.7,
+    // Arrow head
+    "M",
+    x + w * 0.3,
+    y + h * 0.5,
+    "L",
+    x + w * 0.5,
+    y + h * 0.7,
+    "L",
+    x + w * 0.7,
+    y + h * 0.5,
+    // Box
+    "M",
+    x,
+    y + h * 0.9,
+    "L",
+    x,
+    y + h,
+    "L",
+    x + w,
+    y + h,
+    "L",
+    x + w,
+    y + h * 0.9,
+  ];
+  return path;
 };
 
-var movingAvg = function(array, countBefore, countAfter) {
-    if (countAfter == undefined) countAfter = 0;
-    const result = [];
-    for (let i = 0; i < array.length; i++) {
-      if (i < (countBefore-1)) {
-        result.push(null);
-        continue;
-      }
-      const subArr = array.slice(Math.max(i - countBefore + 1, 0), Math.min(i + countAfter + 1, array.length));
-      const avg = subArr.reduce((a, b) => a + (isNaN(b) ? 0 : b), 0) / subArr.length;
-      result.push(avg);
+var movingAvg = function (array, countBefore, countAfter) {
+  if (countAfter == undefined) countAfter = 0;
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (i < countBefore - 1) {
+      result.push(null);
+      continue;
     }
-    return result;
+    const subArr = array.slice(
+      Math.max(i - countBefore + 1, 0),
+      Math.min(i + countAfter + 1, array.length)
+    );
+    const avg =
+      subArr.reduce((a, b) => a + (isNaN(b) ? 0 : b), 0) / subArr.length;
+    result.push(avg);
+  }
+  return result;
 };
 
-var parsePort = function(features) {
-
+var parsePort = function (features) {
   var series = features.map((feature) => {
-      datapoint = {
-        date: feature.attributes.date, 
-        portid: feature.attributes.portid,
-        portname: feature.attributes.portname,
-        country: feature.attributes.country,
-        portcalls_tanker: parseInt(feature.attributes.portcalls_tanker),
-        portcalls_cargo: parseInt(feature.attributes.portcalls_cargo),
-        portcalls: parseInt(feature.attributes.portcalls),
-        import: parseFloat(feature.attributes.import),
-        export: parseFloat(feature.attributes.export),
-      }
-      return datapoint;
+    datapoint = {
+      date: feature.attributes.date,
+      portid: feature.attributes.portid,
+      portname: feature.attributes.portname,
+      country: feature.attributes.country,
+      portcalls_tanker: parseInt(feature.attributes.portcalls_tanker),
+      portcalls_cargo: parseInt(feature.attributes.portcalls_cargo),
+      portcalls: parseInt(feature.attributes.portcalls),
+      import: parseFloat(feature.attributes.import),
+      export: parseFloat(feature.attributes.export),
+    };
+    return datapoint;
   });
 
   series.sort((a, b) => a.date - b.date);
 
   //console.log(series);
   return series;
-
 };
 
-var groupPorts = function(data) {
-
+var groupPorts = function (data) {
   const reduced = data.reduce((acc, item) => {
     if (!acc[item.date]) {
       acc[item.date] = {
@@ -80,37 +99,44 @@ var groupPorts = function(data) {
   }, {});
 
   return Object.values(reduced);
-
 };
 
+var generateIndicators = function (series) {
+  series.sort((a, b) => a.date - b.date);
 
-var generateIndicators = function(series) {
-  
-    series.sort((a, b) => a.date - b.date);
+  import_MA = movingAvg(
+    series.map((x) => x.import),
+    ma,
+    0
+  );
+  export_MA = movingAvg(
+    series.map((x) => x.export),
+    ma,
+    0
+  );
+  portcalls_MA = movingAvg(
+    series.map((x) => x.portcalls),
+    ma,
+    0
+  );
 
-    import_MA = movingAvg(series.map(x => x.import), ma, 0);
-    export_MA = movingAvg(series.map(x => x.export), ma, 0);
-    portcalls_MA = movingAvg(series.map(x => x.portcalls), ma, 0);
+  series = series.map(function (feature, i) {
+    feature["import_MA"] = import_MA[i];
+    feature["export_MA"] = export_MA[i];
+    feature["portcalls_MA"] = portcalls_MA[i];
+    if (i > shift) {
+      feature["import_MA_shifted"] = import_MA[i - shift];
+      feature["export_MA_shifted"] = export_MA[i - shift];
+      feature["portcalls_MA_shifted"] = portcalls_MA[i - shift];
+    }
+    return feature;
+  });
 
-    series = series.map(function(feature, i) {
-
-      feature['import_MA'] = import_MA[i];
-      feature['export_MA'] = export_MA[i];
-      feature['portcalls_MA'] = portcalls_MA[i];
-      if (i > shift) {
-        feature['import_MA_shifted'] = import_MA[i-shift];
-        feature['export_MA_shifted'] = export_MA[i-shift];
-        feature['portcalls_MA_shifted'] = portcalls_MA[i-shift];
-      }
-      return feature;
-    });
-
-    //console.log(series);
-    return series;
-
+  //console.log(series);
+  return series;
 };
 
-var parseEvents = function(features) {
+var parseEvents = function (features) {
   var series = features.map((feature) => {
     datapoint = {
       eventid: feature.attributes.eventid,
@@ -118,12 +144,15 @@ var parseEvents = function(features) {
       eventname: feature.attributes.eventname,
       affectedports: feature.attributes.affectedports,
       fromdate: feature.attributes.fromdate,
-      todate: feature.attributes.todate == null ? Date.now() :  feature.attributes.todate
-    }
+      todate:
+        feature.attributes.todate == null
+          ? Date.now()
+          : feature.attributes.todate,
+    };
     return datapoint;
   });
   return series;
-}
+};
 
 var labels = {
   portcalls: {
@@ -133,234 +162,330 @@ var labels = {
   import: {
     yAxis: "Metric Tons",
     title: "Import Volume",
-    name: 'Import Volume'
+    name: "Import Volume",
   },
   export: {
     yAxis: "Metric Tons",
     title: "Export Volume",
-    name: 'Export Volume'
-  }
+    name: "Export Volume",
+  },
 };
 
 var options = {
-      
   chart: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
 
   credits: {
-      enabled: false
+    enabled: false,
   },
 
   legend: {
-    enabled: true
-
+    enabled: true,
   },
 
   plotOptions: {
     series: {
-        dataGrouping: {
-          enabled: false
-        },
-        showInNavigator: true
+      dataGrouping: {
+        enabled: false,
+      },
+      showInNavigator: true,
     },
     column: {
-      stacking: 'normal'
-    }
+      stacking: "normal",
+    },
   },
 
   rangeSelector: {
-      selected: 1
+    selected: 1,
   },
 
   xAxis: {
-      plotBands: []
+    plotBands: [],
   },
 
   exporting: {
-      enabled: true,
-      buttons: {
-          contextButton: {
-              symbol: 'download',
-              text: 'Export Data'
-          }
-      }
+    enabled: true,
+    buttons: {
+      contextButton: {
+        symbol: "download",
+        text: "Export Data",
+      },
+    },
   },
 
-
-  series: []
+  series: [],
 };
 
-
-var createDisruptionAisChart = function(data, chartType="portcalls") {
-
-  options['yAxis'] = {
+var createDisruptionAisChart = function (data, chartType = "portcalls") {
+  options["yAxis"] = {
     title: {
-      text: labels[chartType].yAxis
+      text: labels[chartType].yAxis,
     },
-    opposite: false
-  }
+    opposite: false,
+  };
 
   if (chartType == "portcalls") {
-
     options.series = [
-      { name: "Number of Cargo Ships",
-        data: data.map(x => [x.date, x['portcalls_cargo']]),
-        type: 'column',
+      {
+        name: "Number of Cargo Ships",
+        data: data.map((x) => [x.date, x["portcalls_cargo"]]),
+        type: "column",
         stack: 1,
         tooltip: {
           valueDecimals: 0,
         },
-        color: '#afc5dc',
-        showInLegend: true},
-    { name: "Number of Tanker Ships",
-      data: data.map(x => [x.date, x['portcalls_tanker']]),
-      type: 'column',
-      stack: 1,
-      tooltip: {
-        valueDecimals: 0,
+        color: "#afc5dc",
+        showInLegend: true,
       },
-      color: '#004c97',
-      showInLegend: true}];
-
-  } else {
-    options.series = [{ name: labels[chartType].name,
-                        data: data.map(x => [x.date, x[chartType]]),
-                        type: 'column',
-                        tooltip: {
-                          valueDecimals: 0,
-                        },
-                        color: '#004c97',
-                        showInLegend: true}];
-  }
-
-
-  options.series = options.series.concat([
-  { name: '7-day Moving Average',
-    data: data.map(x => [x.date, x[chartType+"_MA"]]),
-    type: 'line',
-    marker: {
-      enabled: false, // auto
-      lineWidth: 1,
-    },
-    color: '#f3ab0a',
-    tooltip: {
-          valueDecimals: 0,
-      },
-    showInLegend: true          
-  },
-  { name: 'Prior Year: 7-day Moving Average',
-    data: data.slice(shift+1).map(x => [x.date, x[chartType+"_MA_shifted"]]),
-    type: 'line',
-    marker: {
-      enabled: false, // auto
-      lineWidth: 1,
-    },
-    dashStyle: 'Dash',
-    color: '#474747',
-    tooltip: {
-          valueDecimals: 0,
-      },
-    showInLegend: true          
-  }]);
-
-  options['title'] = {
-    text: "Aggregated " + labels[chartType].title
-  };
-
-  var chart = new Highcharts.stockChart('container', options);
-  
-};
-
-
-var createAisChart = function(data, chartType="portcalls") {
-
-  options['title'] = {
-    text: data[0].portname + ": " + labels[chartType].title
-  };
-
-  options['yAxis'] = {
-    title: {
-      text: labels[chartType].yAxis
-    },
-    opposite: false
-  }
-
-  if (chartType == "portcalls") {
-
-    options.series = [
-      { name: "Number of Cargo Ships",
-        data: data.map(x => [x.date, x['portcalls_cargo']]),
-        type: 'column',
+      {
+        name: "Number of Tanker Ships",
+        data: data.map((x) => [x.date, x["portcalls_tanker"]]),
+        type: "column",
         stack: 1,
         tooltip: {
           valueDecimals: 0,
         },
-        color: '#afc5dc',
-        showInLegend: true},
-    { name: "Number of Tanker Ships",
-      data: data.map(x => [x.date, x['portcalls_tanker']]),
-      type: 'column',
-      stack: 1,
+        color: "#004c97",
+        showInLegend: true,
+      },
+    ];
+  } else {
+    options.series = [
+      {
+        name: labels[chartType].name,
+        data: data.map((x) => [x.date, x[chartType]]),
+        type: "column",
+        tooltip: {
+          valueDecimals: 0,
+        },
+        color: "#004c97",
+        showInLegend: true,
+      },
+    ];
+  }
+
+  options.series = options.series.concat([
+    {
+      name: "7-day Moving Average",
+      data: data.map((x) => [x.date, x[chartType + "_MA"]]),
+      type: "line",
+      marker: {
+        enabled: false, // auto
+        lineWidth: 1,
+      },
+      color: "#f3ab0a",
       tooltip: {
         valueDecimals: 0,
       },
-      color: '#004c97',
-      showInLegend: true}];
-
-  } else {
-    options.series = [{ name: labels[chartType].name,
-                        data: data.map(x => [x.date, x[chartType]]),
-                        type: 'column',
-                        tooltip: {
-                          valueDecimals: 0,
-                        },
-                        color: '#004c97',
-                        showInLegend: true}];
-  }
-
-
-  options.series = options.series.concat([
-  { name: '7-day Moving Average',
-    data: data.map(x => [x.date, x[chartType+"_MA"]]),
-    type: 'line',
-    marker: {
-      enabled: false, // auto
-      lineWidth: 1,
+      showInLegend: true,
     },
-    color: '#f3ab0a',
-    tooltip: {
-          valueDecimals: 0,
+    {
+      name: "Prior Year: 7-day Moving Average",
+      data: data
+        .slice(shift + 1)
+        .map((x) => [x.date, x[chartType + "_MA_shifted"]]),
+      type: "line",
+      marker: {
+        enabled: false, // auto
+        lineWidth: 1,
       },
-    showInLegend: true          
-  },
-  { name: 'Prior Year: 7-day Moving Average',
-    data: data.slice(shift+1).map(x => [x.date, x[chartType+"_MA_shifted"]]),
-    type: 'line',
-    marker: {
-      enabled: false, // auto
-      lineWidth: 1,
+      dashStyle: "Dash",
+      color: "#474747",
+      tooltip: {
+        valueDecimals: 0,
+      },
+      showInLegend: true,
     },
-    dashStyle: 'Dash',
-    color: '#474747',
-    tooltip: {
-          valueDecimals: 0,
-      },
-    showInLegend: true          
-  }]);
+  ]);
 
-  var chart = new Highcharts.stockChart('container', options);  
+  options["title"] = {
+    text: "Aggregated " + labels[chartType].title,
+  };
+
+  var chart = new Highcharts.stockChart("container", options);
 };
 
+var createAisChart = function (data, chartType = "portcalls") {
+  options["title"] = {
+    text: data[0].portname + ": " + labels[chartType].title,
+  };
 
+  options["yAxis"] = {
+    title: {
+      text: labels[chartType].yAxis,
+    },
+    opposite: false,
+  };
 
+  if (chartType == "portcalls") {
+    options.series = [
+      {
+        name: "Number of Cargo Ships",
+        data: data.map((x) => [x.date, x["portcalls_cargo"]]),
+        type: "column",
+        stack: 1,
+        tooltip: {
+          valueDecimals: 0,
+        },
+        color: "#afc5dc",
+        showInLegend: true,
+      },
+      {
+        name: "Number of Tanker Ships",
+        data: data.map((x) => [x.date, x["portcalls_tanker"]]),
+        type: "column",
+        stack: 1,
+        tooltip: {
+          valueDecimals: 0,
+        },
+        color: "#004c97",
+        showInLegend: true,
+      },
+    ];
+  } else {
+    options.series = [
+      {
+        name: labels[chartType].name,
+        data: data.map((x) => [x.date, x[chartType]]),
+        type: "column",
+        tooltip: {
+          valueDecimals: 0,
+        },
+        color: "#004c97",
+        showInLegend: true,
+      },
+    ];
+  }
 
+  options.series = options.series.concat([
+    {
+      name: "7-day Moving Average",
+      data: data.map((x) => [x.date, x[chartType + "_MA"]]),
+      type: "line",
+      marker: {
+        enabled: false, // auto
+        lineWidth: 1,
+      },
+      color: "#f3ab0a",
+      tooltip: {
+        valueDecimals: 0,
+      },
+      showInLegend: true,
+    },
+    {
+      name: "Prior Year: 7-day Moving Average",
+      data: data
+        .slice(shift + 1)
+        .map((x) => [x.date, x[chartType + "_MA_shifted"]]),
+      type: "line",
+      marker: {
+        enabled: false, // auto
+        lineWidth: 1,
+      },
+      dashStyle: "Dash",
+      color: "#474747",
+      tooltip: {
+        valueDecimals: 0,
+      },
+      showInLegend: true,
+    },
+  ]);
 
+  var chart = new Highcharts.stockChart("container", options);
+};
+
+var createAisYoYChart = function (data, chartType = "portcalls") {
+  options["title"] = {
+    text: data[0].country + ": " + "Port Calls by Container Ships, 2025",
+  };
+
+  options["yAxis"] = {
+    title: {
+      text: labels[chartType].yAxis,
+    },
+    opposite: false,
+  };
+
+  if (chartType == "portcalls") {
+    options.series = [
+      {
+        name: "Number of Cargo Ships",
+        data: data.map((x) => [x.date, x["portcalls_cargo"]]),
+        type: "column",
+        stack: 1,
+        tooltip: {
+          valueDecimals: 0,
+        },
+        color: "#afc5dc",
+        showInLegend: true,
+      },
+      {
+        name: "Number of Tanker Ships",
+        data: data.map((x) => [x.date, x["portcalls_tanker"]]),
+        type: "column",
+        stack: 1,
+        tooltip: {
+          valueDecimals: 0,
+        },
+        color: "#004c97",
+        showInLegend: true,
+      },
+    ];
+  } else {
+    options.series = [
+      {
+        name: labels[chartType].name,
+        data: data.map((x) => [x.date, x[chartType]]),
+        type: "column",
+        tooltip: {
+          valueDecimals: 0,
+        },
+        color: "#004c97",
+        showInLegend: true,
+      },
+    ];
+  }
+
+  options.series = options.series.concat([
+    {
+      name: "7-day Moving Average",
+      data: data.map((x) => [x.date, x[chartType + "_MA"]]),
+      type: "line",
+      marker: {
+        enabled: false, // auto
+        lineWidth: 1,
+      },
+      color: "#f3ab0a",
+      tooltip: {
+        valueDecimals: 0,
+      },
+      showInLegend: true,
+    },
+    {
+      name: "Prior Year: 7-day Moving Average",
+      data: data
+        .slice(shift + 1)
+        .map((x) => [x.date, x[chartType + "_MA_shifted"]]),
+      type: "line",
+      marker: {
+        enabled: false, // auto
+        lineWidth: 1,
+      },
+      dashStyle: "Dash",
+      color: "#474747",
+      tooltip: {
+        valueDecimals: 0,
+      },
+      showInLegend: true,
+    },
+  ]);
+
+  var chart = new Highcharts.stockChart("container", options);
+};
 
 /// TESTS
 
 //console.log(labels);
 
 //console.log(movingAvg([1,2,3,1,1,1], 2));
-
-
