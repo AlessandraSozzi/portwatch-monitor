2848726babf53f3831a32789ecd7ec7c4786ddc4
Alessandra Sozzi <alessandra.sozzi@gmail.com>
2025-05-02 15:29:47 -0400

    Update portwatch-ais-charts.js

--- portwatch-ais-charts.js
+++ portwatch-ais-charts.js
@@ -99,7 +99,7 @@ var parseRegion = function (features) {
       portcalls_container_7MA_yoy:
         parseFloat(feature.attributes.portcalls_container_7MA_yoy_doy) * 100,
       portcalls_container_15MA_yoy:
-        parseFloat(feature.attributes.portcalls_container_15MA_yoy_doy) * 100,
+        parseFloat(feature.attributes.portcalls_container_15MA_yoy_do) * 100,
     };
     return datapoint;
   });
