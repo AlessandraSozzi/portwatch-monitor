f49e6402b7127e2ff06f224bd0795a2c0a43ffff
Alessandra Sozzi <alessandra.sozzi@gmail.com>
2025-05-06 13:20:01 -0400

    Update portwatch-ais-charts.js

--- portwatch-ais-charts.js
+++ portwatch-ais-charts.js
@@ -72,7 +72,7 @@ var growthRate = function (array, countBefore) {
 var parsePort = function (features) {
   var series = features.map((feature) => {
     datapoint = {
-      date: featurefeature.attributes.date,
+      date: feature.attributes.date,
       portid: feature.attributes.portid,
       portname: feature.attributes.portname,
       country: feature.attributes.country,
