d971f6b5577ed6299b70d53bea9278f278ba1eb0
Alessandra Sozzi <alessandra.sozzi@gmail.com>
2026-01-28 12:50:48 -0500

    Update portwatch-ais-charts.js

--- portwatch-ais-charts.js
+++ portwatch-ais-charts.js
@@ -73,6 +73,90 @@ var growthRate = function (array, countBefore) {
   return result;
 };
 
+
+var generateNowcastSeries = function (features, ma=3, gr=12) {
+  var series = features.map((feature) => {
+    datapoint = {
+      country: feature.attributes.region,
+      ISO3: feature.attributes.ISO3,
+      date: Date.parse(feature.attributes.date),
+      import_volume: parseFloat(feature.attributes.volume_import_total),
+      export_volume: parseFloat(feature.attributes.volume_export_total),
+      import_value: parseFloat(feature.attributes.value_import_total),
+      export_value: parseFloat(feature.attributes.value_export_total),
+    };
+    return datapoint;
+  });
+
+  series.sort((a, b) => a.date - b.date);
+
+  
+  import_value_MA = movingAvg(
+    series.map((x) => x.import_value),
+    ma,
+    0
+  );
+  export_value_MA = movingAvg(
+    series.map((x) => x.export_value),
+    ma,
+    0
+  );
+  import_volume_MA = movingAvg(
+    series.map((x) => x.import_volume),
+    ma,
+    0
+  );
+  export_volume_MA = movingAvg(
+    series.map((x) => x.export_volume),
+    ma,
+    0
+  );
+
+  import_value_GR = growthRate(
+    series.map((x) => x.import_value),
+    gr
+  ).slice(gr, series.length);
+  export_value_GR = growthRate(
+    series.map((x) => x.export_value),
+    gr
+  ).slice(gr, series.length);
+  import_volume_GR = growthRate(
+    series.map((x) => x.import_volume),
+    gr
+  ).slice(gr, series.length);
+  export_volume_GR = growthRate(
+    series.map((x) => x.export_volume),
+    gr
+  ).slice(gr, series.length);
+
+  import_value_GR_MA = movingAvg(import_value_GR, ma, 0);
+  export_value_GR_MA = movingAvg(export_value_GR, ma, 0);
+  import_volume_GR_MA = movingAvg(import_volume_GR, ma, 0);
+  export_volume_GR_MA = movingAvg(export_volume_GR, ma, 0);
+
+  series = series.map(function (feature, i) {
+    feature["import_value_MA"] = import_value_MA[i];
+    feature["export_value_MA"] = export_value_MA[i];
+    feature["import_volume_MA"] = import_volume_MA[i];
+    feature["export_volume_MA"] = export_volume_MA[i];
+    if (i >= gr) {
+      feature["import_value_GR"] = import_value_GR[i - gr];
+      feature["export_value_GR"] = export_value_GR[i - gr];
+      feature["import_volume_GR"] = import_volume_GR[i - gr];
+      feature["export_volume_GR"] = export_volume_GR[i - gr];
+      // Moving Average of growth rates
+      feature["import_value_GR_MA"] = import_value_GR_MA[i - gr];
+      feature["export_value_GR_MA"] = export_value_GR_MA[i - gr];
+      feature["import_volume_GR_MA"] = import_volume_GR_MA[i - gr];
+      feature["export_volume_GR_MA"] = export_volume_GR_MA[i - gr];
+    }
+    return feature;
+  });
+
+  //console.log(series);
+  return series;
+};
+
 var parsePort = function (features) {
   var series = features.map((feature) => {
     datapoint = {
